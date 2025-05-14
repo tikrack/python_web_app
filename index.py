d495387f914e48aaf0507e0c47c8bf944f7f05b1
tikrack <tikrackcode@gmail.com>
2025-05-14 15:42:37 +0330

    feat(handle): handle the code with webwiew

--- index.py
+++ index.py
@@ -1,22 +1,9 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
+import webview
 
-class BrowserApp(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        
-        self.setWindowTitle("مرورگر وب سایت")
-        self.setGeometry(100, 100, 1024, 768)
-        
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://example.com"))
-        
-        self.setCentralWidget(self.browser)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = BrowserApp()
-    window.show()
-    sys.exit(app.exec_())
+webview.create_window(
+    "HarmonyCMS Control Panel",
+    "https://cpanel.harmonycms.ir",
+    width=1024,
+    height=768
+)
+webview.start()
